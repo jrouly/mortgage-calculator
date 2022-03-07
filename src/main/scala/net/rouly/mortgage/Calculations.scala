a8fc03515784eb406c873839855beff600b40b6b
Jean Michel Rouly <michel.rouly@gmail.com>
2022-03-06 22:28:52 -0500

    Estimate property tax values and add printing

--- src/main/scala/net/rouly/mortgage/Calculations.scala
+++ src/main/scala/net/rouly/mortgage/Calculations.scala
@@ -27,11 +27,21 @@ object Calculations {
     val downPaymentPercent = downPaymentAmount / homeValue
 
     val loanPrinciple = homeValue - downPaymentAmount
-    val loanPayment = Loan.monthlyPayment(loanPrinciple, mortgageRate, mortgageTerm)
-    val propertyTax = propertyTaxRate * homeValue
-    val monthlyPropertyTax = propertyTax / 12
+    val monthlyLoanRepayment = Loan.monthlyPayment(loanPrinciple, mortgageRate, mortgageTerm)
+    val annualPropertyTax = propertyTaxRate * homeValue
+    val monthlyPropertyTax = annualPropertyTax / 12
+    val monthlyPayment = monthlyLoanRepayment + monthlyPropertyTax
 
-    loanPayment + monthlyPropertyTax + hoa
+    println(s"INPUT")
+    println(s"  home value: $$${homeValue.toCurrency}")
+    println(s"  down payment: $$${downPaymentAmount.toCurrency} (${downPaymentPercent.toPercent}%)")
+    println(s"  loan principle: $$${loanPrinciple.toCurrency}")
+    println(s"OUTPUT")
+    println(s"  total monthly payment: $$${monthlyPayment.toCurrency}")
+    println(s"    - loan repayment: $$${monthlyLoanRepayment.toCurrency}")
+    println(s"    - property tax: $$${monthlyPropertyTax.toCurrency}")
+
+    monthlyPayment
   }
 
   /** Determine maximum affordable home price.
@@ -53,10 +63,52 @@ object Calculations {
   ): BigDecimal = {
     val monthlyGrossIncome = annualGrossIncome / 12
     val monthlyPayment = monthlyGrossIncome * debtIncomeRatio
-    val principle =
-      Loan.principleFromMonthlyPayment(monthlyPayment, mortgageRate, mortgageTerm)
+    val monthlyPropertyTax = estimateMonthlyPropertyTax(
+      monthlyPayment = monthlyPayment,
+      downPayment = downPayment,
+      mortgageRate = mortgageRate,
+      mortgageTerm = mortgageTerm,
+      propertyTaxRate = propertyTaxRate
+    )
+    val monthlyLoanRepayment = monthlyPayment - monthlyPropertyTax
+    val loanPrinciple = Loan.principleFromMonthlyPayment(monthlyLoanRepayment, mortgageRate, mortgageTerm)
+    val homeValue = loanPrinciple + downPayment
+    val downPaymentPercent = downPayment / homeValue
 
-    principle + downPayment
+    println(s"INPUT")
+    println(s"  annual gross income: $$${annualGrossIncome.toCurrency}")
+    println(s"  monthly gross income: $$${monthlyGrossIncome.toCurrency}")
+    println(s"  debt/income ratio: ${debtIncomeRatio.toPercent}%")
+    println(s"  down payment: $$${downPayment.toCurrency} (${downPaymentPercent.toPercent}%)")
+    println(s"OUTPUT")
+    println(s"  total monthly payment: $$${monthlyPayment.toCurrency}")
+    println(s"    - loan repayment: $$${monthlyLoanRepayment.toCurrency}")
+    println(s"    - property tax: $$${monthlyPropertyTax.toCurrency} (estimate)")
+    println(s"  loan principle: $$${loanPrinciple.toCurrency}")
+    println(s"  home value: $$${homeValue.toCurrency}")
+
+    homeValue
+  }
+
+  private def estimateMonthlyPropertyTax(
+    monthlyPayment: BigDecimal,
+    downPayment: BigDecimal,
+    mortgageRate: BigDecimal,
+    mortgageTerm: Period,
+    propertyTaxRate: BigDecimal
+  ): BigDecimal = {
+    val estimates = for (propertyTax <- 0 to monthlyPayment.intValue) yield {
+      val loanRepayment = monthlyPayment - propertyTax
+      val principle = Loan.principleFromMonthlyPayment(loanRepayment, mortgageRate, mortgageTerm)
+      val homeValue = principle + downPayment
+
+      val annualPropertyTax = homeValue * propertyTaxRate
+      val monthlyPropertyTax = annualPropertyTax / 12
+
+      propertyTax -> (monthlyPropertyTax - propertyTax).abs
+    }
+
+    estimates.minBy { case (propertyTax, delta) => delta }._1
   }
 
   /** Compute total tax deduction based on legal limits and input amounts. */
@@ -65,6 +117,4 @@ object Calculations {
     val propertyTaxDeduction = propertyTax.min(10 * 1000) // 2021 limit of 10k
     interestDeduction + propertyTaxDeduction
   }
-
-  private def percent(bd: BigDecimal): BigDecimal = (bd * 100).setScale(0, RoundingMode.HALF_UP)
 }
